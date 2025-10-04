f4adbca7700e4c1b3a2203cd0c44e44769e6f5c0
Alivin <wangyongjie@mediinfo.com.cn>
2025-10-04 16:47:52 +0800

    fix: partners logo img src

--- src/pages/partners/index.tsx
+++ src/pages/partners/index.tsx
@@ -18,14 +18,14 @@ const partnersData: { [year: number]: YearData } = {
     '协办单位': [
       {
         name: '北京中关村创业大街科技服务有限公司',
-        logo: '/api/lark/file/CtENbPL80osQwsx8dRxc2DVqnue.webp',
+        logo: '/images/partners/placeholder.webp',
         level: '协办单位'
       }
     ],
     '白金赞助': [
       {
         name: '浪潮 KaiwuDB & KWDB 社区',
-        logo: '/api/lark/file/Sg6Ubpp72oVLj3xHuVycwH8anme.webp',
+        logo: '/img/partners/inno.webpSg6Ubpp72oVLj3xHuVycwH8anme.webp',
         url: 'https://www.kaiwudb.com/',
         level: '白金赞助'
       }
@@ -33,72 +33,72 @@ const partnersData: { [year: number]: YearData } = {
     '金牌赞助': [
       {
         name: '华为',
-        logo: '/api/lark/file/Yeimb7EoSoatVhx2tWTcOuEdnvf.png',
+        logo: '/img/partners/inno.webpYeimb7EoSoatVhx2tWTcOuEdnvf.png',
         level: '金牌赞助'
       }
     ],
     '银牌赞助': [
       {
         name: '蚂蚁开源',
-        logo: '/api/lark/file/Pw5pb1bqloC1UkxhffqcchNcnRf.jpg',
+        logo: '/img/partners/inno.webpPw5pb1bqloC1UkxhffqcchNcnRf.jpg',
         url: 'https://opensource.antgroup.com/',
         level: '银牌赞助'
       },
       {
         name: '亚马逊云科技',
-        logo: '/api/lark/file/RXt8bsDM2oAumTxdLtpcqf24nfc.webp',
+        logo: '/img/partners/inno.webpRXt8bsDM2oAumTxdLtpcqf24nfc.webp',
         level: '银牌赞助'
       },
       {
         name: 'OceanBase',
-        logo: '/api/lark/file/DUW8bgxVSowZwfxezLyc3hxKnhM.png',
+        logo: '/img/partners/inno.webpDUW8bgxVSowZwfxezLyc3hxKnhM.png',
         level: '银牌赞助'
       },
       {
         name: 'openKylin',
-        logo: '/api/lark/file/Ui2qbfaG7oWsc0xaXsgcfG0XnAc.png',
+        logo: '/img/partners/inno.webpUi2qbfaG7oWsc0xaXsgcfG0XnAc.png',
         level: '银牌赞助'
       }
     ],
     '星牌赞助': [
       {
         name: '安势信息',
-        logo: '/api/lark/file/IIXwb9K2xorE0kx1wMxconQfnL3.png',
+        logo: '/img/partners/inno.webpIIXwb9K2xorE0kx1wMxconQfnL3.png',
         level: '星牌赞助'
       },
       {
         name: '醋溜科技',
-        logo: '/api/lark/file/AtDVbNbIjotWifxRSMDckRiSnIe.jpg',
+        logo: '/img/partners/inno.webpAtDVbNbIjotWifxRSMDckRiSnIe.jpg',
         url: 'https://www.culiu-tech.com/',
         level: '星牌赞助'
       },
       {
         name: '北京大成（上海）律师事务所',
-        logo: '/api/lark/file/S8dNb7s1Co9coOxZHdjchNUKn7U.webp',
+        logo: '/img/partners/inno.webpS8dNb7s1Co9coOxZHdjchNUKn7U.webp',
         level: '星牌赞助'
       },
       {
         name: '迪码科技',
-        logo: '/api/lark/file/SmATb2SQSoFPTExGHzpc8nHVnSh.png',
+        logo: '/img/partners/inno.webpSmATb2SQSoFPTExGHzpc8nHVnSh.png',
         url: 'https://www.dimatec.com.cn/portal/aboutus.html',
         level: '星牌赞助'
       },
       {
         name: '立创开源硬件平台',
-        logo: '/api/lark/file/Qd1rbWdY0oRNLVxP2oPckFCNnnh.png',
+        logo: '/img/partners/inno.webpQd1rbWdY0oRNLVxP2oPckFCNnnh.png',
         url: 'https://oshwhub.com/',
         level: '星牌赞助'
       },
       {
         name: 'Seafile',
-        logo: '/api/lark/file/DVGobTYa5oY2a7xObeDcGsX6ngd.png',
+        logo: '/img/partners/inno.webpDVGobTYa5oY2a7xObeDcGsX6ngd.png',
         level: '星牌赞助'
       }
     ],
     '战略合作社区': [
       {
         name: 'SegmentFault 思否',
-        logo: '/api/lark/file/M6DxbRm5coWH0yxMsZscl4ennMb.png',
+        logo: '/img/partners/inno.webpM6DxbRm5coWH0yxMsZscl4ennMb.png',
         url: 'https://segmentfault.com/',
         level: '战略合作社区'
       }
@@ -108,67 +108,67 @@ const partnersData: { [year: number]: YearData } = {
     '协办单位': [
       {
         name: '电子科技大学',
-        logo: '/api/lark/file/QwQ2bKMA6ohCsIx6ta2cy1ntn9f.png',
+        logo: '/img/partners/inno.webpQwQ2bKMA6ohCsIx6ta2cy1ntn9f.png',
         level: '协办单位'
       },
       {
         name: '成都傲世信息技术有限公司',
-        logo: '/api/lark/file/Y9JfbURWfoT9bMx4NS4cjE7tnCg.jpg',
+        logo: '/img/partners/inno.webpY9JfbURWfoT9bMx4NS4cjE7tnCg.jpg',
         level: '协办单位'
       }
     ],
     '指导单位': [
       {
         name: '成都市高新区科技创新局',
-        logo: '/api/lark/file/F67tbzUfro3oEtxpszXc9UDqnAg.jpg',
+        logo: '/img/partners/inno.webpF67tbzUfro3oEtxpszXc9UDqnAg.jpg',
         level: '指导单位'
       }
     ],
     '白金赞助': [
       {
         name: '华为',
-        logo: '/api/lark/file/Yeimb7EoSoatVhx2tWTcOuEdnvf.png',
+        logo: '/img/partners/inno.webpYeimb7EoSoatVhx2tWTcOuEdnvf.png',
         level: '白金赞助'
       },
       {
         name: '浪潮',
-        logo: '/api/lark/file/Xxy5brcmkoMwzUx0ftfcHg3onth.jpg',
+        logo: '/img/partners/inno.webpXxy5brcmkoMwzUx0ftfcHg3onth.jpg',
         level: '白金赞助'
       }
     ],
     '金牌赞助': [
       {
         name: 'GOSIM',
-        logo: '/api/lark/file/SLN1bXIbgo0jIkxe09hckbwunlh.png',
+        logo: '/img/partners/inno.webpSLN1bXIbgo0jIkxe09hckbwunlh.png',
         level: '金牌赞助'
       }
     ],
     '银牌赞助': [
       {
         name: 'NEAR',
-        logo: '/api/lark/file/Ro9Ebrq3DoiCShx9HU6cas2inOc.png',
+        logo: '/img/partners/inno.webpRo9Ebrq3DoiCShx9HU6cas2inOc.png',
         url: 'https://github.com/near',
         level: '银牌赞助'
       },
       {
         name: 'OceanBase',
-        logo: '/api/lark/file/DUW8bgxVSowZwfxezLyc3hxKnhM.png',
+        logo: '/img/partners/inno.webpDUW8bgxVSowZwfxezLyc3hxKnhM.png',
         level: '银牌赞助'
       },
       {
         name: '亚马逊云科技',
-        logo: '/api/lark/file/RXt8bsDM2oAumTxdLtpcqf24nfc.webp',
+        logo: '/img/partners/inno.webpRXt8bsDM2oAumTxdLtpcqf24nfc.webp',
         level: '银牌赞助'
       },
       {
         name: '蚂蚁开源',
-        logo: '/api/lark/file/Pw5pb1bqloC1UkxhffqcchNcnRf.jpg',
+        logo: '/img/partners/inno.webpPw5pb1bqloC1UkxhffqcchNcnRf.jpg',
         url: 'https://opensource.antgroup.com/',
         level: '银牌赞助'
       },
       {
         name: '麒麟软件有限公司',
-        logo: '/api/lark/file/UCa3b33iFosc3ixklVWcThEjnQf.jpg',
+        logo: '/img/partners/inno.webpUCa3b33iFosc3ixklVWcThEjnQf.jpg',
         url: 'https://www.kylinos.cn/',
         level: '银牌赞助'
       }
@@ -178,44 +178,44 @@ const partnersData: { [year: number]: YearData } = {
     '白金赞助': [
       {
         name: '开务数据库',
-        logo: '/api/lark/file/UT1TbHWvBot1VtxwYULcw93ZnDE.png',
+        logo: '/img/partners/inno.webpUT1TbHWvBot1VtxwYULcw93ZnDE.png',
         level: '白金赞助'
       },
       {
         name: 'VMWare',
-        logo: '/api/lark/file/Nw5jbJ0rnoMrQ6xZCisc8Ymkn4e.jpg',
+        logo: '/img/partners/inno.webpNw5jbJ0rnoMrQ6xZCisc8Ymkn4e.jpg',
         level: '白金赞助'
       },
       {
         name: '百度开源',
-        logo: '/api/lark/file/DPpWbmiLvoD4eoxjc4yccqrcnlc.png',
+        logo: '/img/partners/inno.webpDPpWbmiLvoD4eoxjc4yccqrcnlc.png',
         level: '白金赞助'
       },
       {
         name: '华为',
-        logo: '/api/lark/file/Yeimb7EoSoatVhx2tWTcOuEdnvf.png',
+        logo: '/img/partners/inno.webpYeimb7EoSoatVhx2tWTcOuEdnvf.png',
         level: '白金赞助'
       },
       {
         name: '微众银行',
-        logo: '/api/lark/file/N94ObM2XQoxDPTxiVbwceWsCndc.png',
+        logo: '/img/partners/inno.webpN94ObM2XQoxDPTxiVbwceWsCndc.png',
         level: '白金赞助'
       },
       {
         name: '字节跳动',
-        logo: '/api/lark/file/J3AfbuFCiorvy5xogovcAcwznYg.jpg',
+        logo: '/img/partners/inno.webpJ3AfbuFCiorvy5xogovcAcwznYg.jpg',
         level: '白金赞助'
       }
     ],
     '金牌赞助': [
       {
         name: '亚马逊云科技',
-        logo: '/api/lark/file/RXt8bsDM2oAumTxdLtpcqf24nfc.webp',
+        logo: '/img/partners/inno.webpRXt8bsDM2oAumTxdLtpcqf24nfc.webp',
         level: '金牌赞助'
       },
       {
         name: '微软',
-        logo: '/api/lark/file/An2EbHu0XoZEvZxbXzgcUerLnLb.jpg',
+        logo: '/img/partners/inno.webpAn2EbHu0XoZEvZxbXzgcUerLnLb.jpg',
         level: '金牌赞助'
       }
     ]
